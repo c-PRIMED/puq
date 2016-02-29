09ad04de5526960f9a67eea387f05fd1ec4e1292
Martin Hunt <huntmartinm@gmail.com>
2016-02-28 22:10:03 -0500

    fix dump_hdf5 to always output json

--- puqutil/__init__.py
+++ puqutil/__init__.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+
 def dump_hdf5(name, v, desc=''):
     np.set_printoptions(threshold=np.nan)
-    print 'HDF5:%s:5FDH' % repr({'name': name, 'desc': desc, 'value':v})
+    line = pickle({'name': name, 'desc': desc, 'value': v})
+    print 'HDF5:%s:5FDH' % line
