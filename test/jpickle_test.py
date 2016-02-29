cfd7e875fb893d8899a680e4570370149ea8d79d
Martin Hunt <huntmartinm@gmail.com>
2016-02-28 22:07:19 -0500

    add 2d array test

--- test/jpickle_test.py
+++ test/jpickle_test.py
@@ -7,21 +7,21 @@ Some basic tests of the JSON pickle handlers
 
 def test_numpy_int_array():
     a = np.array([1], dtype=np.int)
-    assert a.dtype == np.int    
+    assert a.dtype == np.int
     a = unpickle(pickle(a))
     assert a.dtype == np.int
 
 def test_numpy_int8_array():
     a = np.array([1], dtype=np.int8)
-    assert a.dtype == np.int8  
+    assert a.dtype == np.int8
     a = unpickle(pickle(a))
     assert a.dtype == np.int8
 
 def test_numpy_int16_array():
     a = np.array([1], dtype=np.int16)
-    assert a.dtype == np.int16    
+    assert a.dtype == np.int16
     a = unpickle(pickle(a))
-    assert a.dtype == np.int16    
+    assert a.dtype == np.int16
 
 def test_numpy_int32_array():
     a = np.array([1], dtype=np.int32)
@@ -63,13 +63,15 @@ def test_numpy_int16():
 
 def test_numpy_float():
     a = np.float(3.1415926)
-    assert a == unpickle(pickle(a)) 
-    
+    assert a == unpickle(pickle(a))
+
 def test_numpy_bool():
     a = np.bool(True)
     assert unpickle(pickle(a)) == True
     a = np.bool(False)
     assert unpickle(pickle(a)) == False
-        
 
-
+def test_2d_array():
+    m = np.arange(1, 5)
+    m = np.meshgrid(m, m, m)
+    assert np.array_equal(m, unpickle(pickle(m)))
