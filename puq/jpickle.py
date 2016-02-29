5d2d4dd7aded44e791bbae66a06b0974a5a83632
Martin Hunt <huntmartinm@gmail.com>
2016-02-28 22:09:12 -0500

    minor cleanup

--- puq/jpickle.py
+++ puq/jpickle.py
@@ -3,15 +3,21 @@ This module implements all the type handlers and wrappers necessary
 to use jsonpickle with PUQ.
 
 This file is part of PUQ
-Copyright (c) 2013 PUQ Authors
+Copyright (c) 2013-2016 PUQ Authors
 See LICENSE file for terms.
 """
+
+#
+# Note: classes must inherit from object to be pickled!!
+#
 
 import os
 import jsonpickle
 import numpy as np
 import sympy
 from scipy.interpolate import Rbf
+from sympy.core.numbers import Float
+
 
 class NumpyFloatHandler(jsonpickle.handlers.BaseHandler):
     def flatten(self, obj, data):
@@ -19,6 +25,7 @@ class NumpyFloatHandler(jsonpickle.handlers.BaseHandler):
 jsonpickle.handlers.registry.register(np.float, NumpyFloatHandler)
 jsonpickle.handlers.registry.register(np.float32, NumpyFloatHandler)
 jsonpickle.handlers.registry.register(np.float64, NumpyFloatHandler)
+
 
 class NumpyIntHandler(jsonpickle.handlers.BaseHandler):
     def flatten(self, obj, data):
@@ -33,63 +40,76 @@ jsonpickle.handlers.registry.register(np.uint16, NumpyIntHandler)
 jsonpickle.handlers.registry.register(np.uint32, NumpyIntHandler)
 jsonpickle.handlers.registry.register(np.uint64, NumpyIntHandler)
 
+
 class NumpyBoolHandler(jsonpickle.handlers.BaseHandler):
     def flatten(self, obj, data):
         return bool(obj)
 jsonpickle.handlers.registry.register(np.bool, NumpyBoolHandler)
 
+
 class SympyAddHandler(jsonpickle.handlers.BaseHandler):
     def flatten(self, obj, data):
         data['value'] = str(obj)
         return data
+
     def restore(self, obj):
         return sympy.S(obj['value'])
 jsonpickle.handlers.registry.register(sympy.Add, SympyAddHandler)
+
 
 class SympyMulHandler(jsonpickle.handlers.BaseHandler):
     def flatten(self, obj, data):
         data['value'] = str(obj)
         return data
+
     def restore(self, obj):
         return sympy.S(obj['value'])
 jsonpickle.handlers.registry.register(sympy.Mul, SympyMulHandler)
 
-from sympy.core.numbers import Float
+
 class SympyFloatHandler(jsonpickle.handlers.BaseHandler):
     def flatten(self, obj, data):
         data['value'] = str(obj)
         return data
+
     def restore(self, obj):
         return sympy.S(obj['value'])
 jsonpickle.handlers.registry.register(Float, SympyFloatHandler)
 
+
 class NumpyArrayHandler(jsonpickle.handlers.BaseHandler):
     def flatten(self, obj, data):
-        #print "arrayhandler flatten", obj.dtype
+        # print "arrayhandler flatten", obj.dtype
         data['value'] = obj.tolist()
         data['dtype'] = str(obj.dtype)
         return data
+
     def restore(self, obj):
-        #print 'arrayhandler restore', obj
+        # print 'arrayhandler restore', obj
         return np.array(obj['value'], dtype=obj['dtype'])
 jsonpickle.handlers.registry.register(np.ndarray, NumpyArrayHandler)
+
 
 # don't try to flatten this.  Will be handled in _reinit_()
 class RbfHandler(jsonpickle.handlers.BaseHandler):
     def flatten(self, obj, data):
         return data
+
     def restore(self, obj):
         return obj
 jsonpickle.handlers.registry.register(Rbf, RbfHandler)
 
+
 def pickle(obj):
     return jsonpickle.encode(obj)
+
 
 def unpickle(st):
     obj = jsonpickle.decode(str(st))
     if hasattr(obj, '_reinit_'):
         obj._reinit_()
     return obj
+
 
 def LoadObj(filename):
     """
@@ -107,6 +127,7 @@ def LoadObj(filename):
     obj = unpickle(f.read())
     f.close()
     return obj
+
 
 def NetObj(addr):
     """
@@ -130,6 +151,7 @@ def NetObj(addr):
 
     return unpickle(val)
 
+
 def write_json(obj, filename):
     """
     Encode an object with json and write it to a file.
@@ -141,9 +163,7 @@ def write_json(obj, filename):
     """
     fn, ext = os.path.splitext(filename)
     if ext and ext != '.json':
-        raise ValueError, "Filename extension needs to be '.json'"
-    filename =  fn + '.json'
+        raise ValueError("Filename extension needs to be '.json'")
+    filename = fn + '.json'
     with open(filename, 'w') as jfile:
         jfile.write(pickle(obj))
-
-
