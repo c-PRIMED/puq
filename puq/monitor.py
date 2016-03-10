650fbacd1f426b3e31665ac2dfb7c83f585912c2
Martin Hunt <huntmartinm@gmail.com>
2016-03-10 00:06:47 -0500

    minor cleanup.  use new class

--- puq/monitor.py
+++ puq/monitor.py
@@ -1,12 +1,15 @@
 from util import vprint
 from logging import info, debug, exception, warning, critical
 
-class Monitor:
+
+class Monitor(object):
     pass
+
 
 class QtMonitor(Monitor):
     def __init__(self, host):
-        raise Exception, 'PyQt not installed'
+        raise Exception('PyQt not installed')
+
 
 class TextMonitor(Monitor):
     def start_job(self, cmd, id):
