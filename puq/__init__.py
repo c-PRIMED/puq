9c3e131ce732e76dc954bf61201d7de55e82c218
Martin Hunt <huntmartinm@gmail.com>
2016-03-28 09:27:58 -0400

    add Calibrate

--- puq/__init__.py
+++ puq/__init__.py
@@ -16,7 +16,7 @@ from util import Callback, dump_hdf5
 from response import Function, ResponseFunc, SampledFunc
 from jpickle import pickle, unpickle, NetObj, LoadObj, write_json
 from analyzer import analyzer
-from calibrate import calibrate
+from calibrate import calibrate, Calibrate
 from sparse_grid import sgrid
 from ._version import get_versions
 __version__ = get_versions()['version']
